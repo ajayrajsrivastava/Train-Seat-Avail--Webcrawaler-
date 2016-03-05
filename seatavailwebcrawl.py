711a20c284ddd71152ea0f34653edf06234c97a8
Ajay Raj Srivastava <ajayisready@gmail.com>
2016-03-06 05:21:12 +0530

    Update seatavailwebcrawl.py

--- seatavailwebcrawl.py
+++ seatavailwebcrawl.py
@@ -1,8 +1,15 @@
-
 import requests
 from bs4 import BeautifulSoup
 import pynotify
 from time import sleep
+
+def crawl(url):
+    sc=requests.get(url)
+    soup=BeautifulSoup(sc.content,'lxml')
+    for row in soup.find_all('div',attrs={"id" : "first1"}):
+        x=row.text
+    return x
+
 
 try:
     trainno=input('Enter Train Number :- ')
@@ -10,20 +17,19 @@ try:
     ars=raw_input('Enter Arrival Station Code :- ')
     deps=raw_input('Enter Departure Station code :- ')
     cls=raw_input('Enter Class Code eg SL-Sleeper 3A-Third AC etc :-')
-    x='XXX'
+    
     ars.lower()
     deps.lower()
     url='http://www.railyatri.in/seat-availability/'+(str)(trainno)+'-'+ars+'-'+deps+'?'+'journey_class='+cls+'&journey_date='+date+'&quota=GN'
-    while x[0]=='A':
-        sc=requests.get(url)
-        soup=BeautifulSoup(sc.content,'lxml')
-        for row in soup.find_all('div',attrs={"id" : "first1"}):
-            x=row.text
 
+    x=crawl(url)
+    while x[0]=='A': #wil show notifications until and unless seats are available
+        x=crawl(url)
         pynotify.init('test')
         n = pynotify.Notification('        Availability - '+str(x))
         n.show()
 
+    x=crawl(url)
     pynotify.init('test')
     n = pynotify.Notification('        Availability - '+str(x))
     n.show()
